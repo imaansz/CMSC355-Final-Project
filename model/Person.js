058fcaf32efca3b3b7512df252ae7f5747fe0692
imaansz <ims44@umd.edu>
2025-12-09 19:49:45 -0500

    added mongoose file

---
+++ model/Person.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const personSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+   },
+   anime: {
+      type: String,
+      required: true,
+   }
+});
+
+const Person = mongoose.model("Person", personSchema);
+module.exports = Person;
