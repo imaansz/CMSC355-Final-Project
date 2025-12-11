48121a0f5fb26d8eb13341e2fe73ad1b9995a346
imaansz <ims44@umd.edu>
2025-12-10 22:56:51 -0500

    added post to js file

--- animeFinder.js
+++ animeFinder.js
@@ -34,21 +34,64 @@ app.get("/recommendationForm", (request, response) => {
     response.render("recommendationForm");
 }); 
 
-app.post("/processRecommendationForm", async (request, response) => {
-    const variables = {
-        genre: request.body?.genre ?? "NONE",
-        anime: ""//ADD API HERE
-    }
+app.post("/processRecommendationForm", (request, response) => {
+    let genre = request.body.genre;
+    fetch(`https://kitsu.io/api/edge/anime?filter[[categories]=${genre}&page[limit]=5&page[offset]=0`, {
+            method: 'GET',
+            headers: {
+               'Accept': 'application/vnd.api+json',
+               'Content-Type': 'application/vnd.api+json'
+            }
+         })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            processObject(data);
+         })
+        .catch(error => console.error(error));
+        
+        function processObject({data, links, meta}) { 
+            console.log("** Data Retrieved\n");
+            let namesList = "";
+            let count = 0;
+            for (entry in data){
+               namesList += `<strong>Name:</strong> ${data[count].attributes.canonicalTitle} <br><strong>Descrption:</strong> ${data[count].attributes.description}<br> <strong>Average Rating:</strong> ${data[count].attributes.averageRating} <br><br>`;
+               count += 1;
+            }
+            let variables = {
+                genre : request.body?.genre ?? "NONE",
+                anime: namesList
+            };
+            response.render("processRecommendationForm", variables);
+        }
+});
 
-    response.render("processRecommendationForm", variables);
+
+app.get("/userLookupForm", (request, response) => { 
+    response.render("userLookupForm");
+});
+
+app.post("/userLookupPost", async (request, response) => { 
+    let userName = request.body.name;
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+        const people = await Person.find({ name: `${userName}` }); //find all the people with matching names
+        let animeList = "";
+        for (entry in people) {
+            animeList += `${entry.anime}<br>`
+        }
+        let variables = {
+            user: userName,
+            animes: animeList
+        };
+        response.render("userLookupPost", variables);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 app.get("/searchByTitle", (request, response) => { 
     response.render("searchByTitle");
-});
-
-app.get("/userLookupForm", (request, response) => { 
-    response.render("userLookupForm");
 });
 
 app.post("/listedTitles", (request, response) => {
